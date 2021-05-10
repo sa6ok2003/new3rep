53cda84cf8d841fa7abf8756a5a84031742a219f
sa6ok2003 <73820004+sa6ok2003@users.noreply.github.com>
2021-05-10 08:52:35 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 
-TOKEN = '1659619578:AAH0rNg9p4ZNy0Fj53XFeQEA5N2Esj4Pzpo'
+TOKEN = 
 memory_storage = MemoryStorage()
 
 bot = Bot(token=TOKEN)
